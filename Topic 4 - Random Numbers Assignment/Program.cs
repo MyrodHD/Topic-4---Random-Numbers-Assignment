96cdf0b6dc917d074743253393fb4fcb0d9ecc7b
MyrodHD <Krysalink@outlook.com>
2026-02-09 13:10:30 -0500

    fixed a bit of crash potential

--- Topic 4 - Random Numbers Assignment/Program.cs
+++ Topic 4 - Random Numbers Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Topic_4___Random_Numbers_Assignment
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Topic_4___Random_Numbers_Assignment
 {
     internal class Program
     {
@@ -17,27 +19,44 @@
             Console.WriteLine("Provide me with a minimum value: ");
             min = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine();
-            Console.WriteLine(Generator.Next(min, max));
-            Console.WriteLine(Generator.Next(min, max));
-            Console.WriteLine(Generator.Next(min, max));
-            Console.WriteLine(Generator.Next(min, max));
-            Console.WriteLine(Generator.Next(min, max));
-            Console.WriteLine();
-            Console.WriteLine("Press Enter to continue");
-            Console.ReadLine();
-            Console.Clear();
+            if (max < min)
+            {
+                (max, min) = (min, max);
+                Console.WriteLine();
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine(Generator.Next(min, max));
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+            }
 
             //Part 2
             int dice1;
             int dice2;
 
-            dice1 = Generator.Next(0,25);
-            dice2 = Generator.Next(0,25);
+            dice1 = Generator.Next(0, 25);
+            dice2 = Generator.Next(0, 25);
 
-            Console.WriteLine(dice1);
+            Console.WriteLine($"Your first roll is {dice1}");
             Console.WriteLine();
-            Console.WriteLine(dice2);
+            Console.WriteLine($"Your second roll is {dice2}");
             Console.WriteLine();
             Console.WriteLine($"The total of the numbers is: {dice1 + dice2}");
             Console.WriteLine();
@@ -47,7 +66,6 @@
 
             //Part 3
 
-
             int decimalAmount;
             double randmax = (double)max;
             double randmin = (double)min;
@@ -56,13 +74,14 @@
             decimalAmount = Convert.ToInt32(Console.ReadLine());
 
             double number = Generator.NextDouble() * (randmin - randmax) + randmin;
-            number = Math.Round(number,decimalAmount);
+            number = Math.Round(number, decimalAmount);
 
             double number2 = Generator.NextDouble() * (randmin - randmax) + randmin;
             number2 = Math.Round(number2, decimalAmount);
 
             double number3 = Generator.NextDouble() * (randmin - randmax) + randmin;
             number3 = Math.Round(number3, decimalAmount);
+
 
             Console.WriteLine(number);
             Console.WriteLine(number2);
